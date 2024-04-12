a59ccaf12a16b9e546e156926a412debbe014250
Hatem Nabli <hatem.nabli@actemium.com>
2024-04-12 14:02:23 +0200

    Test: test for - sprintf and vsprintf methods

---
+++ test/src/StringUtilsTests.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file StringUtils.cpp
+ * 
+ * This module contains unit Tests of the StringUtils functions 
+ * 
+ * © 2024 by Hatem Nabli
+*/
+
+#include <gtest\gtest.h>
+#include <StringUtils\StringUtils.hpp>
+
+std::string vsprintfHelper(char const* format, ...) {
+    va_list args;
+    va_start(args, format);
+    const auto result = StringUtils::vsprintf("%s, %s!", args);
+    va_end(args);
+    return result;
+}
+
+TEST(StringUtilsTests, vsprintf__Test) {
+    ASSERT_EQ("Hello, World!", vsprintfHelper("%s, %s!", "Hello", "World"));
+}
+
+TEST(StringUtilsTests, sprintfStandardCLibrarysVersionBehaves__Test) {
+    ASSERT_EQ("Hello, World!", StringUtils::sprintf("%s, %s!", "Hello", "World"));
+}
+
+TEST(StringUtilsTests, sprintfReturnsStringComparabletoCppString) {
+    const std::string expectedOutput = "The answer is 47.";
+    ASSERT_EQ(expectedOutput, StringUtils::sprintf("The answer is %d.", 47));
+}
+
