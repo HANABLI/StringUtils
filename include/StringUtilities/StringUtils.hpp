378905c677119c027dd998270b4aada4f4ee8026
Hatem Nabli <hatem.nabli@actemium.com>
2024-04-12 13:55:43 +0200

    Feat: vsprintf function tha it constructs the string dynamically

--- include/StringUtilities/StringUtils.hpp
+++ include/StringUtilities/StringUtils.hpp
@@ -7,21 +7,31 @@
  * 
  * © 2024 by Hatem Nabli
 */
-
+#include <stdarg.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <ctype.h>
 #include <memory>
-
+#include <string>
+#include <limits>
 
 
 namespace StringUtils {
 
-
-
+    
     /**
-     * This function is equivalent to the sprintf function in the standard C
-     * library, except that it constructs the string dynamically and
+     * This fucntion is equivalent to the sprintf function in the standard C
+     * library, except tha it constructs the string dynamically and
      * returns it as a std::string value
     */
     std::string sprintf(const char* format, ...);
+
+    /**
+     * This fucntion is equivalent to the vsprintf function in the standard C
+     * library, except tha it constructs the string dynamically and
+     * returns it as a std::string value
+    */
+    std::string vsprintf(const char* format, va_list args);
 
     
 }
--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -18,5 +18,21 @@ namespace StringUtils
         return vsprintf(format, args);
     }
 
-    
+    std::string vsprintf(const char* format, va_list args) {
+       va_list argsCopy;
+       va_copy(argsCopy, args);
+       const int required = vsnprintf(nullptr, 0, format, args);
+       va_end(args);
+       if (required < 0) {
+            va_end(argsCopy);
+            return "";
+       } 
+       std::vector< char > buffer(required + 1);
+       const int result = vsnprintf(&buffer[0], required + 1, format, argsCopy);
+       va_end(argsCopy);
+       if (result < 0) {
+        return "";
+       }
+       return std::string(buffer.begin(), buffer.begin() + required);
+    }
 } // namespace StringUtils
